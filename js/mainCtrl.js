a0b224a75a2131a52c57b6e1a4e0b8e3b6537be7
mcgagnier <mcgagnier@yahoo.com>
2017-02-14 20:01:56 -0700

    basics

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,12 +1,19 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+  //The getMessages function will call the getMessages method on the messageService
+  // object.
+  // //You'll then save the result of that request to your controller's
+  // $scope as messages ($scope.messages)
+
+  $scope.getMessages = function () {
+      mainService.getMessages().then(function(response) {
+          $scope.messages = response;
+      })
+  }
 
 
-
-
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+  //The postMessage function will take whatever the user typed in
+  // (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
